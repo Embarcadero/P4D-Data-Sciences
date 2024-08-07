551db5923fad9a9e8384169233dd64ddf9f87d12
Jim McKeeth <jim@mckeeth.org>
2024-08-07 02:06:59 -0600

    removed /bl from msbuild for backwards compatibility

--- buildall.bat
+++ buildall.bat
@@ -1,5 +1,5 @@
 @echo off
 call rsvars.bat
 call get-depends.bat
-msbuild /bl /p:BuildGroup=RunTime P4DDataSciencesComponentSuite.groupproj
+msbuild /p:BuildGroup=RunTime P4DDataSciencesComponentSuite.groupproj
 python.exe convert-depends-to-sbom.py
